442d531866f9d53e17e0aa8f47fcd876ee2b47a0
rebecaamaciel <142433287+rebecaamaciel@users.noreply.github.com>
2024-07-26 12:17:14 -0300

    ds

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const botoes = document.querySelectorAll('.opcoes');
-const abas = document.querySelectorAll('aba-conteudo');
+const abas = document.querySelectorAll('.aba-conteudo');
 
 
 for (let i = 0; i < botoes.length; i++) {
     botoes.onclick = function () {
         for (let j = 0; j < botoes.length; j++) {
             botoes[j].classList.remove('ativo');
-            abas[j].classList.remove('ativo');
+            abas[j].classList.remove('ativa');
         }
          botoes[i].classList.add('ativo');
-         abas[i].classList.add('ativo');
+         abas[i].classList.add('ativa');
         
     }
     
