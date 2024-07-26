b533b13352771fdb59984f76c5d7ea9b5a9fac3c
rebecaamaciel <142433287+rebecaamaciel@users.noreply.github.com>
2024-07-26 12:10:33 -0300

    fd

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const abas = document.querySelectorAll('aba-conteudo');
 
 
 for (let i = 0; i < botoes.length; i++) {
+    botoes[i].classList.add('ativo');
     for (let j = 0; j < botoes.length; j++) {
-        botoes[i].classList.add('ativo');
         botoes[j].classList.remove('ativo');
         
     }
