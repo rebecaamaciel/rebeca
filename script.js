1a9d256d9943196b34fc5d3b6a6193afb6accb52
rebecaamaciel <142433287+rebecaamaciel@users.noreply.github.com>
2024-07-26 12:06:46 -0300

    sds

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 const botoes = document.querySelectorAll('.opcoes');
+const abas = document.querySelectorAll('aba-conteudo');
+
+
+for (let i = 0; i < botoes.length; i++) {
+    for (let j = 0; j < botoes.length; j++) {
+        botoes[i].add
+        
+    }
+    
+}
